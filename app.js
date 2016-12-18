90448005452693923f5ae1da71f68a097b13f80b
Walter Berggren <walter.berggren@gmail.com>
2016-12-18 17:42:40 +0200

    Change anonymous function to arrow syntax

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const TOKEN = process.env.TOKEN
 
-app.get('/', function (req, res) {
+app.get("/", (req, res) => {
   res.send('Hello World')
 })
 
