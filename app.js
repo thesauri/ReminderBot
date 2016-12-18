6502e2c957d02d29b0d2198a63916988d2f98769
Walter Berggren <walter.berggren@gmail.com>
2016-12-18 17:23:09 +0200

    Attempt to set webhook and log the result

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express")
+const request = require("request")
 const app = express()
 const PORT = process.env.PORT || 5000
+const TOKEN = process.env.TOKEN
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get(`/${TOKEN}`, (req, res) => {
+  console.log(req)
+})
+
 app.listen(PORT)
 console.log(`Listening on ${PORT}`)
+
+const options = {
+  url: `https://api.telegram.org/bot${TOKEN}/setWebhook`,
+  headers: {
+    "url": `https://bobthereminderbot.herokuapp.com/${TOKEN}`
+  }
+}
+
+request(options, (error, response, body) => {
+  console.log(response)
+})
