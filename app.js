f22d559ffcfe0f3b31021e1b2511061e8a4fab6e
Walter Berggren <walter.berggren@gmail.com>
2016-12-18 17:48:37 +0200

    Add body-parse library

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const request = require("request")
+const bodyParser = require("body-parser")
 const app = express()
 const PORT = process.env.PORT || 5000
 const TOKEN = process.env.TOKEN
+
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   res.send('Hello World')
